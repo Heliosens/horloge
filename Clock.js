54919901506edba8389b8271efda24f9ebdc612d
Sylvie-B <unsolo@hotmail.fr>
2021-12-13 15:18:50 +0100

    create two clock

--- Clock.js
+++ Clock.js
@@ -1,22 +1,47 @@
 // create needle : type = hour, min, sec
 let Clock = function (){
 
-    this.createNeedle = function (target, w, h, color, type){
+
+
+    this.createNeedle = function (target, w, h, hUnit, color, type, imgUrl = ""){
+        // create frame needle
+        let frame = document.createElement('div');
+        frame.style.height = (2 * h) + hUnit;
         // create needle
         let needle = document.createElement('div');
         needle.style.width = w;
-        needle.style.height = h;
+        needle.style.height = h + hUnit;
         needle.style.backgroundColor = color;
-        needle.style.position = "absolue";
+        needle.style.position = "absolute";
         needle.style.transformOrigin = "center bottom";
+        frame.appendChild(needle);
+
         switch (type){
             case 'sec' :
                 setInterval(function (){
                     let r = new Date().getSeconds();
                     needle.style.transform = "rotate(" + r*6 + "deg)";
                 }, 1000)
+                break;
+            case 'min' :
+                setInterval(function (){
+                    let r = new Date().getMinutes();
+                    needle.style.transform = "rotate(" + r*6 + "deg)";
+                }, 1000)
+                break;
+            case 'hour' :
+                setInterval(function (){
+                    let r = new Date().getHours();
+                    needle.style.transform = "rotate(" + r*30 + "deg)";
+                }, 1000)
+                break;
         }
-        document.getElementById(target).appendChild(needle);
+        let place = document.getElementById(target);
+        place.style.position = "relative";
+        place.style.display = "flex";
+        place.style.justifyContent = "center";
+        place.style.alignItems = "center";
+        place.appendChild(frame);
     }
 
 }
--- appTime.js
+++ appTime.js
@@ -1,6 +1,12 @@
 let target = document.querySelector('#target');
 
 let clock = new Clock();
-clock.createNeedle("target", "3px", "10vh", "blue", "sec");
+clock.createNeedle("target", "3px", "10","vh", "blue", "sec", "");
+clock.createNeedle("target", "6px", "7", "vh","black", "min", "");
+clock.createNeedle("target", "9px", "5", "vh", "red", "hour", "");
 
+let clock2 = new Clock();
+clock2.createNeedle("clockPlace","2px", "15", "vh", "black", "sec", "");
+clock2.createNeedle("clockPlace","3px", "10", "vh", "black", "min", "");
+clock2.createNeedle("clockPlace","4px", "5", "vh", "black", "hour", "");
 
